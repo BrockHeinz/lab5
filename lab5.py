3bd5317923ccf6fa2db2920f2c2fee9b00d55e77
Brock Heinz <bch285@nau.edu>
2020-10-26 18:31:19 -0700

    added a-q

--- lab5.py
+++ lab5.py
@@ -1,5 +1,135 @@
 # Authors: Luke Frazer and Brock Heinz
 
 def main():
+    sample = """
+#######
+#######
+#######
+#######
+#######
+"""
+    a = """
+   A   
+  A A  
+ AAAAA 
+A     A
+A     A
+"""
+    b = """
+BBBBB  
+B    B 
+BBBBB  
+B    B 
+BBBBB  
+"""
+    c = """
+  CCCC 
+CC     
+C      
+CC     
+  CCCC 
+"""
+    d = """
+DDDDD  
+D    D 
+D    D 
+D    D 
+DDDDD  
+"""
+    e = """
+EEEEEE 
+E      
+EEEEEE 
+E      
+EEEEEE 
+"""
+    f = """
+FFFFFF 
+F      
+FFFFFF 
+F      
+F      
+"""
+    g = """
+ GGGG  
+G      
+G  GGG 
+G    G 
+ GGGG  
+"""
+    h = """
+H     H
+H     H
+HHHHHHH
+H     H
+H     H
+"""
+    i = """
+IIIIIII
+   I   
+   I   
+   I   
+IIIIIII
+"""
+    j = """
+JJJJJJJ
+   J   
+   J   
+J  J   
+ JJ    
+"""
+    k = """
+K   KK 
+K KK   
+KK     
+K KK   
+K   KK 
+"""
+    l = """
+L      
+L      
+L      
+L      
+LLLLLL 
+"""
+    m = """
+M    M 
+MM  MM 
+M MM M 
+M MM M 
+M    M 
+"""
+    n = """
+N   N  
+NN  N  
+N N N  
+N  NN  
+N   N  
+"""
+    o = """
+ OOOO  
+O    O 
+O    O 
+O    O 
+ OOOO  
+"""
+    p = """
+PPPPP  
+P    P 
+PPPPP  
+P      
+P      
+"""
+    q = """
+ QQQQ  
+Q    Q 
+Q    Q 
+ QQQQ  
+     Q 
+"""
+    
+    print(sample[0:16])
+
+main()
 
 
